16c8a5b39f282b2f353f097fc9cee278e78ec8f0
cameronyoung94 <68090343+cameronyoung94@users.noreply.github.com>
2021-01-19 21:38:26 -0500

    made the use dark mode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = (initialValues) => {
+ const [darkMode, setDarkMode] = useLocalStorage('dark', initialValues)
+
+ const toggleMode = () => {
+  setDarkMode(!darkMode)
+ }
+
+ return [darkMode, toggleMode]
+
+}
+
+export default useDarkMode
